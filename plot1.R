556a5510a781510b0c4aa735173665ddc352fbc3
Chenxi Li <sisi881022@gmail.com>
2014-08-08 19:08:58 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -10,5 +10,6 @@ project1<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
 datetime<-strptime(paste(project1$Date, project1$Time), "%Y-%m-%d %H:%M:%S")
 
 #plot1
-
+png("plot1.png")
 hist(project1[,3],xlab="Global Active Power (kilowatts)",main=" Global Active Power",col="red")
+dev.off()
