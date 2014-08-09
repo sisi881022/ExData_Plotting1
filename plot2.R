952152617c649f5e8962a1571ed968ad79821f9b
Chenxi Li <sisi881022@gmail.com>
2014-08-09 18:55:53 -0400

    Completed the R code for plot 2

--- plot2.R
+++ plot2.R
@@ -4,13 +4,11 @@ colnames(powerConsumption)
 #Convert date
 powerConsumption$Date<- as.Date(powerConsumption$Date,"%d/%m/%Y")
 #subset data 
-project1<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
-
-
-datetime<-strptime(paste(project1$Date, project1$Time), "%Y-%m-%d %H:%M:%S")
-
+dat<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
+#convert the datetime
+datetime<-strptime(paste(dat$Date, dat$Time), "%Y-%m-%d %H:%M:%S")
 
 #plot2
 png("plot2.png")
-plot(datetime,project1[,3],type="l",xlab="",ylab="Global Active Power (kilowatts)")
+plot(datetime,dat[,3],type="l",xlab="",ylab="Global Active Power (kilowatts)")
 dev.off()
