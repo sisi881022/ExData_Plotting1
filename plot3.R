930f1f1b6ab4a67d573d081a393e916f158321d8
Chenxi Li <sisi881022@gmail.com>
2014-08-09 18:57:36 -0400

    Completed the R code for plot 3

--- plot3.R
+++ plot3.R
@@ -4,15 +4,14 @@ colnames(powerConsumption)
 #Convert date
 powerConsumption$Date<- as.Date(powerConsumption$Date,"%d/%m/%Y")
 #subset data 
-project1<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
-
-
-datetime<-strptime(paste(project1$Date, project1$Time), "%Y-%m-%d %H:%M:%S")
+dat<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
+#convert the datetime
+datetime<-strptime(paste(dat$Date, dat$Time), "%Y-%m-%d %H:%M:%S")
 
 #plot3
 png("plot3.png")
-plot(datetime, project1[,7],  type="l", col="black", ylab="Energy sub metering", xlab="")
-lines(datetime,project1[,8], type="l", col="red")
-lines(datetime,project1[,9], type="l", col="blue")
-legend("topright", legend=c("Sub_metering_1", "Sub-metering_2", "Sub_meteing_3"), lwd=2,col=c("black", "red", "blue"))
+plot(datetime, dat[,7],  type="l", col="black", ylab="Energy sub metering", xlab="")
+lines(datetime,dat[,8], type="l", col="red")
+lines(datetime,dat[,9], type="l", col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub-metering_2", "Sub_meteing_3"), lwd=2,col=c("black", "red", "blue")) # add legend to plot
 dev.off()
