96622ff8056c2ecbea1445dc5185f488eecac8cc
Chenxi Li <sisi881022@gmail.com>
2014-08-09 18:54:43 -0400

    Completed the R code for plot 1

--- plot1.R
+++ plot1.R
@@ -1,15 +1,12 @@
 # Read in Data
 PowerConsumption<- read.csv("household_power_consumption.txt",header=T,sep=";", na.strings = "?")
 colnames(powerConsumption)
-#Convert date
+#Convert dates
 powerConsumption$Date<- as.Date(powerConsumption$Date,"%d/%m/%Y")
 #subset data 
-project1<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
-
-
-datetime<-strptime(paste(project1$Date, project1$Time), "%Y-%m-%d %H:%M:%S")
+dat<- subset(powerConsumption,Date>="2007-02-01" & Date<= "2007-02-02")
 
 #plot1
 png("plot1.png")
-hist(project1[,3],xlab="Global Active Power (kilowatts)",main=" Global Active Power",col="red")
+hist(dat[,3],xlab="Global Active Power (kilowatts)",main=" Global Active Power",col="red")
 dev.off()
